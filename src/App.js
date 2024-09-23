e1660a8fadecd20a4cbb018b2ab42e540a9cb7c4
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-09-23 15:54:39 +0530

    faq-s component made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { questions } from "./Data/faqQuestion";
 
 function App() {
+  let [showAns, setShowAns] = useState(questions[0].id);
+
+
+  let [count, setCount] = useState(1);
+  let countDisplay = () => {
+    setCount(count + 1);
+  };
+
+  let [pstatus, pStatus] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="border border-black p-2"
+        placeholder="Enter text"
+        type={pstatus ? "text" : "password"}
+      />
+      <button
+        className="rounded-sm bg-red-800"
+        onClick={() => pStatus(!pstatus)}
+      >
+        {pstatus ? "hide" : "show"}
+      </button>
+      <p>hello from aamir hussain</p>
+      <div>{count}</div>
+      <button className="rounded-md bg-red-500" onClick={countDisplay}>
+        click
+      </button>
+
+      <div>
+        <h1>Frequently Asked Questions(FAQs)</h1>
+        <div className="faqouter">
+          {questions.map((faqItems, i) => {
+            return (
+              <div className="faqItems">
+                <h2 onClick={()=>setShowAns(faqItems.id)}>{faqItems.question}</h2>
+                <p className={showAns === faqItems.id ? 'activeAns' : ''}>{faqItems.answer}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Data/faqQuestion.jsx
@@ -0,0 +1,26 @@
+export const questions = [
+    {
+        id:1,
+        question: 'Popular Articles',
+        answer : 'Tincidunt sagittis consequat commodo ex mollis aenean nulla velit. Efficitur placerat accumsan taciti ultricies mattis habitant quisque. Rutrum aenean ipsum per; ultrices porta pretium. Eleifend fusce quisque purus aliquet feugiat porttitor in. Nostra habitant ultrices auctor diam sodales ridiculus eleifend fermentum montes. Netus primis sit nulla inceptos nisi eros id.'
+
+    },
+    {
+        id: 2,
+        question : 'Fix problems & request removals',
+        answer : 'Gravida porttitor felis mollis enim hendrerit aliquam, tempor curabitur dolor. Aenean libero venenatis per porta dolor tristique suspendisse dignissim. Nunc neque orci potenti aliquam convallis quis. Cras volutpat dignissim; arcu posuere sapien bibendum at ut. Mi magna tristique magnis a ut mollis augue nullam. Natoque fringilla nostra eu tincidunt himenaeos nunc elit magnis. Magnis mi est; dis ridiculus nisl vel dictum.'
+
+    },
+    {
+        id : 3,
+        question : 'Brose the web',
+        answer : 'Luctus cubilia maecenas quam a erat; blandit nascetur primis. Dapibus nisi posuere porttitor dolor augue vestibulum urna tellus. Primis risus massa augue habitant sagittis proin pharetra nunc. Curae turpis dictumst neque integer cras ex. Hac praesent ut natoque nulla ad aliquam maecenas. '
+
+    },
+    {
+        id : 4,
+        question : 'Search on your phone or tablet',
+        answer : 'Aliquam platea velit non, dis scelerisque venenatis est. Sociosqu porttitor sapien orci interdum habitant velit bibendum. Porta semper dapibus ullamcorper ultricies porttitor leo vestibulum vestibulum lacinia. Eros tristique vitae suspendisse per est interdum lacus.'
+
+    },
+]
